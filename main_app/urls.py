f098da58d796fc36019cc122d1b67d5cf3d1d17e
sebastians9517 <sebastians9517@gmail.com>
2021-02-10 09:01:35 -0600

    finish functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.LanguagesList.as_view(), name='index'),
+    path('', views.home, name='home'),
+    path('languages/', views.languages_index, name='index'),
     path('languages/create/', views.LanguagesCreate.as_view(), name='create'),
     path('languages/<int:pk>/update/', views.LanguagesUpdate.as_view(), name='update'),
     path('languages/<int:pk>/delete/', views.LanguagesDelete.as_view(), name='delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Languages
 
 # Create your views here.
-class LanguagesList(ListView):
-    model = Languages
+def home(request):
+    return render(request, 'home.html')
 
-class LanguagesCreate(CreateView):
+@login_required
+def languages_index(request):
+    # languages = Languages.objects.all()
+    languages = Languages.objects.filter(user=request.user)
+    return render(request, 'languages/index.html', { 'languages': languages })
+
+
+class LanguagesCreate(LoginRequiredMixin, CreateView):
     model = Languages
     fields = '__all__'
-    success_url = '/'
+    success_url = '/languages'
 
-class LanguagesUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class LanguagesUpdate(LoginRequiredMixin, UpdateView):
     model = Languages
-    success_url = '/'
+    success_url = '/languages'
     fields = '__all__'
 
-class LanguagesDelete(DeleteView):
+class LanguagesDelete(LoginRequiredMixin, DeleteView):
     model = Languages
-    success_url = '/'
+    success_url = '/languages'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
